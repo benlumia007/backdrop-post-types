bf5f8e981824885b8cb0704d4e19951e03f9d856
Benjamin Lu <benlumia007@gmail.com>
2019-04-15 16:28:15 -0700

    updated

--- includes/post-types.php
+++ includes/post-types.php
@@ -34,10 +34,16 @@ class CustomPostTypes {
 	protected $posts;
 
 	/**
+	 * Posts.
+	 */
+	protected $categories;
+
+	/**
 	 * Construct.
 	 */
 	public function register() {
 		add_action( 'init', array( $this, 'register_custom_post_types' ) );
+		add_action( 'init', array( $this, 'register_custom_post_types_category' ) );
 	}
 
 	/**
@@ -89,10 +95,54 @@ class CustomPostTypes {
 			'labels'      => $labels,
 			'public'      => true,
 			'has_archive' => true,
+			'menu_icon'   => 'dashicons-category',
 			'supports'    => array( 'title', 'editor', 'thumbnail' ),
+			'taxonomies'  => array( $this->posts[ $type ] ),
 			'show_ui'     => true,
 		);
 
 		$this->posts[ $type ] = array_merge( $labels, $args );
 	}
+
+	public function register_custom_post_types_category( $type ) {
+
+		$labels = array(
+			'name'                       => _x( 'Categories', 'Taxonomy General Name', 'latte' ),
+			'singular_name'              => _x( 'Category', 'Taxonomy Singular Name', 'latte' ),
+			'menu_name'                  => __( 'Categories', 'latte' ),
+			'all_items'                  => __( 'All Categories', 'latte' ),
+			'parent_item'                => __( 'Parent Category', 'latte' ),
+			'parent_item_colon'          => __( 'Parent Category:', 'latte' ),
+			'new_item_name'              => __( 'New Category Name', 'latte' ),
+			'add_new_item'               => __( 'Add New Category', 'latte' ),
+			'edit_item'                  => __( 'Edit Categories', 'latte' ),
+			'update_item'                => __( 'Update Categories', 'latte' ),
+			'view_item'                  => __( 'View Categories', 'latte' ),
+			'separate_items_with_commas' => __( 'Separate categories with commas', 'latte' ),
+			'add_or_remove_items'        => __( 'Add or remove categories', 'latte' ),
+			'choose_from_most_used'      => __( 'Choose from the most used', 'latte' ),
+			'popular_items'              => __( 'Popular Categories', 'latte' ),
+			'search_items'               => __( 'Search Categories', 'latte' ),
+			'not_found'                  => __( 'Not Found', 'latte' ),
+		);
+
+		$args = array(
+			'labels'            => $labels,
+			'hierarchical'      => true,
+			'public'            => true,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'show_in_nav_menus' => false,
+			'show_tagcloud'     => false,
+		);
+		register_taxonomy( $this->posts[ $type ], array( $this->posts[ $type ] ), $args );
+	}
 }
+
+$portfolio = new CustomPostTypes( 'portfolio' );
+$portfolio->create_post_type( 'portfolio', 'Portoflio', 'Portfolios' );
+$portfolio->register();
+
+$portfolio = new CustomPostTypes( 'theme' );
+$portfolio->create_post_type( 'theme', 'Theme', 'Themes' );
+$portfolio->register();
